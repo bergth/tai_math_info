03e938b70dfc6c2098e282561dfd804bec9af4b2
BenAzlay <34835201+BenAzlay@users.noreply.github.com>
2018-04-08 14:12:16 +0200

    J'ai ajouté le #include <vector>

--- Automate.h
+++ Automate.h
@@ -1,17 +1,18 @@
 #ifndef AUTOMATE_H_INCLUDED
 #define AUTOMATE_H_INCLUDED
+#include <vector>
 
 class Automate
 {
     private:
-        // Directement dérivé du cours : définition d'un automate
+        // Directement dÃ©rivÃ© du cours : dÃ©finition d'un automate
         std::vector<Etat*> etats;
         std::vector<Etat*> etatsInitiaux;
         std::vector<Etat*> etatsTerminaux;
-        std::vector<std::tuple<Etat*, Lettre, Etat*>> transitions; // OU : encapsuler les transitions en définissant une classe Transition
+        std::vector<std::tuple<Etat*, Lettre, Etat*>> transitions; // OU : encapsuler les transitions en dÃ©finissant une classe Transition
 
     public:
-        // Constructeur prend en paramètre le fichier contenant les données de l'automate à construire
+        // Constructeur prend en paramÃ¨tre le fichier contenant les donnÃ©es de l'automate Ã  construire
         Automate(const char* file);
         ~Automate();
 };
